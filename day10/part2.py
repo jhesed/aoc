cfed8d40fdf10c58941f77c78456da2e5d0714c6
jhesed <jhesed.tacadena@swapoolabs.com>
2020-12-10 15:51:29 +0800

    master: Little cleanup

--- day10/part2.py
+++ day10/part2.py
@@ -10,40 +10,62 @@ from day10.common import ACCEPTABLE_DIFF
 
 
 def generate_unique_key(joltage: int):
-    """Simply generates a string of key to avoid collision with other
-    numbers."""
+    """Simply generates a string of key to avoid collision with other numbers
+    in e.g. the dictionary.
+
+    :param joltage: e.g. 10
+    :return: e.g. KEY10
+    """
     return f"KEY{joltage}"
 
 
 def count_number_of_ways(
-    data: list, current_joltage: int, current_index: int, info: dict
+    data: list,
+    current_joltage: int,
+    current_index: int,
+    ways_info: dict,
+    max_value: int,
 ) -> int:
-    """Count possible number of ways allowed in rule."""
+    """Count possible number of ways in joltage list."""
 
-    if current_joltage == max(data):
+    if current_joltage == max_value:
         return 1
 
     key = generate_unique_key(joltage=current_joltage)  # e.g. KEY123
-    if key in info:
-        return info[key]
+    if key in ways_info:
+        return ways_info[key]
 
     ways_counter = 0
-    acceptable_joltages = [
-        i
-        for i in data
-        if current_joltage < i <= (current_joltage + ACCEPTABLE_DIFF)
-    ]
-    for aj in acceptable_joltages:
-        ways_counter += count_number_of_ways(data, aj, current_index + 1, info)
+    acceptable_joltages = []
+    for joltage in data[current_index:]:
+        if not (
+            current_joltage < joltage <= (current_joltage + ACCEPTABLE_DIFF)
+        ):
+            # Our list is sorted, so once we get value > than this, next
+            # values will also be the same
+            break
 
-    info[key] = ways_counter
+        acceptable_joltages.append(joltage)
+
+    for aj in acceptable_joltages:
+        ways_counter += count_number_of_ways(
+            data, aj, current_index + 1, ways_info, max_value
+        )
+
+    ways_info[key] = ways_counter
     return ways_counter
 
 
 if __name__ == "__main__":
     with open("data.txt", "r") as _file:
         _inputs = sorted([int(i) for i in _file.read().splitlines()])
+        _max_value = max(_inputs)
+        _ways_info = {}
         answer = count_number_of_ways(
-            data=_inputs, current_joltage=0, current_index=0, info={}
+            data=_inputs,
+            current_joltage=0,
+            current_index=0,
+            ways_info=_ways_info,
+            max_value=_max_value,
         )
         print(f"Answer: {answer}")
